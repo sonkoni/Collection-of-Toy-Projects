fe595a12d57f7ce23766a5a043fec4f271b46ddf
koni <isomorphic111@gmail.com>
2023-05-09 18:40:42 +0900

    ...
    ...

---
+++ Contents/AutoLayout_Animation/AutoLayoutAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TESTTEST
+//
+//  Created by Kwan Hyun Son on 15/12/2019.
+//  Copyright © 2019 Mulgrim Co. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
