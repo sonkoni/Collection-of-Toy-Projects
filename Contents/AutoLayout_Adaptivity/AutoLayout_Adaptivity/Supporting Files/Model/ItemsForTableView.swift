b75e76154379796e9a693ca551e480867454f23b
koni <isomorphic111@gmail.com>
2023-05-10 16:56:58 +0900

    ....

---
+++ Contents/AutoLayout_Adaptivity/AutoLayout_Adaptivity/Supporting Files/Model/ItemsForTableView.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  AutoLayout_Adaptivity
+//
+//  Created by Kwan Hyun Son on 2022/10/10.
+//
+
+import Foundation
+
+typealias SectionItems = (sectionTitle: String, items: Array<Item>)
+
+final class ItemsForTableView {
+    
+    // MARK: - Property
+    static let shared = ItemsForTableView()
+    private(set) var allItems : Array<SectionItems>
+    
+    // MARK: - 생성 & 소멸
+    private init() {
+        let item1 = Item(title:"Swift Sample", detailText:"Swift 코드 베이스")
+        let item2 = Item(title:"Objective-C Sample", detailText:"Objective-C 코드 베이스")
+        allItems = [(sectionTitle: "Basic", items: [item1, item2])]
+    }
+}
