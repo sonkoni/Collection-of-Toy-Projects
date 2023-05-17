79ad72035b581750f0f512fd29af8717fd3b2877
koni <isomorphic111@gmail.com>
2023-05-17 13:52:45 +0900

    ....
    ...

--- Contents/임시파일_이건 지우자/MGUButtons/Empty/IosObjcSample/Supporting Files/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  IosObjcEmpty
-//
-//  Created by Kiro on 2022/11/16.
-//
-
-import Foundation
--- Contents/임시파일_이건 지우자/MGUButtons/Empty/MacObjcSample/File.swift
+++
@@ -1,8 +0,0 @@
-//
-//  File.swift
-//  MacObjcEmpty
-//
-//  Created by Kiro on 2022/11/16.
-//
-
-import Foundation
