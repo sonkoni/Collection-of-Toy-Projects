7653f8b0eaa16b2255566ac4f5ec9ae665ff3fb1
koni <isomorphic111@gmail.com>
2023-06-09 08:51:46 +0900

    ...

--- Contents/AutoLayout_Adaptivity/AutoLayout_Adaptivity/Supporting Files/Extensions/UIView+AutoLayout.swift
+++ Contents/AutoLayout_Adaptivity/AutoLayout_Adaptivity/Supporting Files/Extensions/UIView+AutoLayout.swift
@@ -12,12 +12,12 @@ import UIKit
 public extension UIView {
     @discardableResult
     func mgrPinEdgesToSuperviewEdges()  -> [NSLayoutConstraint] {
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = leadingAnchor.constraint(equalTo: superview!.leadingAnchor)
-        let c2 = trailingAnchor.constraint(equalTo: superview!.trailingAnchor)
-        let c3 = topAnchor.constraint(equalTo: superview!.topAnchor)
-        let c4 = bottomAnchor.constraint(equalTo: superview!.bottomAnchor)
+        let c1 = leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+        let c2 = trailingAnchor.constraint(equalTo: superview.trailingAnchor)
+        let c3 = topAnchor.constraint(equalTo: superview.topAnchor)
+        let c4 = bottomAnchor.constraint(equalTo: superview.bottomAnchor)
         c1.isActive = true
         c2.isActive = true
         c3.isActive = true
@@ -27,12 +27,12 @@ public extension UIView {
     
     @discardableResult
     func mgrPinEdgesToSuperviewLayoutMarginsGuide()  -> [NSLayoutConstraint] {
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = leadingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.leadingAnchor)
-        let c2 = trailingAnchor.constraint(equalTo: superview!.layoutMarginsGuide.trailingAnchor)
-        let c3 = topAnchor.constraint(equalTo: superview!.layoutMarginsGuide.topAnchor)
-        let c4 = bottomAnchor.constraint(equalTo: superview!.layoutMarginsGuide.bottomAnchor)
+        let c1 = leadingAnchor.constraint(equalTo: superview.layoutMarginsGuide.leadingAnchor)
+        let c2 = trailingAnchor.constraint(equalTo: superview.layoutMarginsGuide.trailingAnchor)
+        let c3 = topAnchor.constraint(equalTo: superview.layoutMarginsGuide.topAnchor)
+        let c4 = bottomAnchor.constraint(equalTo: superview.layoutMarginsGuide.bottomAnchor)
         c1.isActive = true
         c2.isActive = true
         c3.isActive = true
@@ -42,12 +42,12 @@ public extension UIView {
     
     @discardableResult
     func mgrPinEdgesToSuperviewSafeAreaLayoutGuide()  -> [NSLayoutConstraint] {
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = leadingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.leadingAnchor)
-        let c2 = trailingAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.trailingAnchor)
-        let c3 = topAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.topAnchor)
-        let c4 = bottomAnchor.constraint(equalTo: superview!.safeAreaLayoutGuide.bottomAnchor)
+        let c1 = leadingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.leadingAnchor)
+        let c2 = trailingAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.trailingAnchor)
+        let c3 = topAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.topAnchor)
+        let c4 = bottomAnchor.constraint(equalTo: superview.safeAreaLayoutGuide.bottomAnchor)
         c1.isActive = true
         c2.isActive = true
         c3.isActive = true
@@ -57,12 +57,12 @@ public extension UIView {
     
     @discardableResult
     func mgrPinEdgesToSuperviewCustomMargins(_ customMargins: UIEdgeInsets)  -> [NSLayoutConstraint] {  // 5,5,5,5 이면 안쪽으로 5만큼 파고든다.
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = topAnchor.constraint(equalTo: superview!.topAnchor, constant: customMargins.top)
-        let c2 = leadingAnchor.constraint(equalTo: superview!.leadingAnchor, constant: customMargins.left)
-        let c3 = superview!.trailingAnchor.constraint(equalTo: trailingAnchor, constant: customMargins.right)
-        let c4 = superview!.bottomAnchor.constraint(equalTo: bottomAnchor, constant: customMargins.bottom)
+        let c1 = topAnchor.constraint(equalTo: superview.topAnchor, constant: customMargins.top)
+        let c2 = leadingAnchor.constraint(equalTo: superview.leadingAnchor, constant: customMargins.left)
+        let c3 = superview.trailingAnchor.constraint(equalTo: trailingAnchor, constant: customMargins.right)
+        let c4 = superview.bottomAnchor.constraint(equalTo: bottomAnchor, constant: customMargins.bottom)
         c1.isActive = true
         c2.isActive = true
         c3.isActive = true
@@ -72,10 +72,10 @@ public extension UIView {
     
     @discardableResult
     func mgrPinHorizontalEdgesToSuperviewEdges()  -> [NSLayoutConstraint] { // leading, trailing만 super view에 맞춘다.
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = leadingAnchor.constraint(equalTo: superview!.leadingAnchor)
-        let c2 = trailingAnchor.constraint(equalTo: superview!.trailingAnchor)
+        let c1 = leadingAnchor.constraint(equalTo: superview.leadingAnchor)
+        let c2 = trailingAnchor.constraint(equalTo: superview.trailingAnchor)
         c1.isActive = true
         c2.isActive = true
         return [c1, c2]
@@ -83,10 +83,10 @@ public extension UIView {
     
     @discardableResult
     func mgrPinVerticalEdgesToSuperviewEdges()  -> [NSLayoutConstraint] { // top, bottom만 super view에 맞춘다.
-        assert(superview != nil, "Superview 는 nil 이어서는 안된다.")
+        guard let superview = self.superview else { fatalError("Superview 는 nil 이어서는 안된다.") }
         self.translatesAutoresizingMaskIntoConstraints = false
-        let c1 = topAnchor.constraint(equalTo: superview!.topAnchor)
-        let c2 = bottomAnchor.constraint(equalTo: superview!.bottomAnchor)
+        let c1 = topAnchor.constraint(equalTo: superview.topAnchor)
+        let c2 = bottomAnchor.constraint(equalTo: superview.bottomAnchor)
         c1.isActive = true
         c2.isActive = true
         return [c1, c2]
